6cb765666593bba667b503dd4a3b4b3e0c3006cb
Mathieu YamiShadow <mathieu.nibas@gmail.com>
2019-01-19 08:49:22 +0100

    Feat(Navigation): Change order items on Navigation

--- BookDetector/navigation/MainTabNavigator.js
+++ BookDetector/navigation/MainTabNavigator.js
@@ -1,60 +1,63 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+import React from "react";
+import { Platform } from "react-native";
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
 
-import TabBarIcon from '../components/TabBarIcon';
-import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import TabBarIcon from "../components/TabBarIcon";
+import BarcodeScanner from "../components/BarcodeScanner";
+import LinksScreen from "../screens/LinksScreen";
+import SettingsScreen from "../screens/SettingsScreen";
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
+const CameraStack = createStackNavigator({
+  Camera: BarcodeScanner
 });
 
-HomeStack.navigationOptions = {
-  tabBarLabel: 'Home',
+CameraStack.navigationOptions = {
+  tabBarLabel: "Camera",
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
       name={
-        Platform.OS === 'ios'
-          ? `ios-information-circle${focused ? '' : '-outline'}`
-          : 'md-information-circle'
+        Platform.OS === "ios"
+          ? `ios-information-circle${focused ? "" : "-outline"}`
+          : "md-information-circle"
       }
     />
-  ),
+  )
 };
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
+const LastBookStack = createStackNavigator({
+  Links: LinksScreen
 });
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+LastBookStack.navigationOptions = {
+  tabBarLabel: "Last Books",
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
+      name={Platform.OS === "ios" ? "ios-link" : "md-link"}
     />
-  ),
+  )
 };
 
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
+const HistoricStack = createStackNavigator({
+  Settings: SettingsScreen
 });
 
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
+HistoricStack.navigationOptions = {
+  tabBarLabel: "Historic",
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+      name={Platform.OS === "ios" ? "ios-options" : "md-options"}
     />
-  ),
+  )
 };
 
 export default createBottomTabNavigator({
-  HomeStack,
-  LinksStack,
-  SettingsStack,
+  LastBookStack,
+  CameraStack,
+  HistoricStack
 });
